269433a23e16a37fe5fb93b386ec45cea56754d6
Romzo <romzo@ihtut.net>
2018-02-08 22:08:37 +0300

    edit name

--- dist/js/lomzo.js
+++ dist/js/lomzo.js
@@ -1,7 +1,7 @@
 /*
     Lomzo 0.1PA
-    Author: Romzo
-    Website: https://romzo.github.io/lomzo/
+    Author: Lomzo
+    Website: https://lomzo.github.io/lomzo/
     Code released under the MIT License 
     Docs released under Creative Commons
 */
